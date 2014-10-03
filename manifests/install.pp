4ce543252ba031c699ead8966e2f89fb1c5bbcdf
mh <mh@immerda.ch>
2014-10-03 18:21:08 +0200

    make it possible to install from a repository
    not everyone wants to install from the wild wild internet and
    have a local repository in place where they would add the packages
    to install. This change makes it configurable, from where things
    should be installed, but keeping the old behavior.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,37 +1,41 @@
 # == Class: influxdb::install
 # DO NO CALL DIRECTLY
 class influxdb::install {
-  # package source and provider
-  case $::osfamily {
-    'Debian': {
-      $package_provider = 'dpkg'
-      $package_source = $::architecture ? {
-        /64/    => "http://s3.amazonaws.com/influxdb/influxdb_${influxdb::version}_amd64.deb",
-        default => "http://s3.amazonaws.com/influxdb/influxdb_${influxdb::version}_i386.deb",
-      }
-    }
-    'RedHat', 'Amazon': {
-      $package_provider = 'rpm'
-      $package_source = $::architecture ? {
-        /64/    => "http://s3.amazonaws.com/influxdb/influxdb-${influxdb::version}-1.x86_64.rpm",
-        default => "http://s3.amazonaws.com/influxdb/influxdb-${influxdb::version}-1.i686.rpm",
-      }
-    }
-    default: {
-      fail('Only supports Debian or RedHat $::osfamily')
-    }
-  }
-
-  # get the package
-  staging::file { 'influxdb-package':
-    source   => $package_source,
-  }
-
-  # install the package
   package { 'influxdb':
     ensure   => $influxdb::ensure,
-    provider => $package_provider,
-    source   => '/opt/staging/influxdb/influxdb-package',
-    require  => Staging::File['influxdb-package'],
+  }
+  if !$influxdb::install_from_repository {
+    # package source and provider
+    case $::osfamily {
+      'Debian': {
+        $package_provider = 'dpkg'
+        $package_source = $::architecture ? {
+          /64/    => "http://s3.amazonaws.com/influxdb/influxdb_${influxdb::version}_amd64.deb",
+          default => "http://s3.amazonaws.com/influxdb/influxdb_${influxdb::version}_i386.deb",
+        }
+      }
+      'RedHat', 'Amazon': {
+        $package_provider = 'rpm'
+        $package_source = $::architecture ? {
+          /64/    => "http://s3.amazonaws.com/influxdb/influxdb-${influxdb::version}-1.x86_64.rpm",
+          default => "http://s3.amazonaws.com/influxdb/influxdb-${influxdb::version}-1.i686.rpm",
+        }
+      }
+      default: {
+        fail('Only supports Debian or RedHat $::osfamily')
+      }
+    }
+  
+    # get the package
+    staging::file { 'influxdb-package':
+      source   => $package_source,
+    }
+  
+    # install the package
+    Package['influxdb']{
+      provider => $package_provider,
+      source   => '/opt/staging/influxdb/influxdb-package',
+      require  => Staging::File['influxdb-package'],
+    }
   }
 }

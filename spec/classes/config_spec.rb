1a93815871d68719d997d8594372c0e0c9053604
Florian Thiel <florian.thiel@epost-dev.de>
2014-11-23 22:46:37 +0100

    [Test] complete "disable all input plugins by default" test cases

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -6,11 +6,21 @@ describe 'influxdb::config' do
     'include influxdb'
   }
 
-  context 'with default configuration' do
-    it 'should disable all input plugins' do
+  context 'with default configuration it should disable all input plugins' do
+    it 'graphite' do
       should contain_ini_setting('graphite_enabled').with_section('input_plugins.graphite')
       should contain_ini_setting('graphite_enabled').with_setting('enabled')
       should contain_ini_setting('graphite_enabled').with_value('false')
+    end
+    it 'collectd' do
+      should contain_ini_setting('collectd_enabled').with_section('input_plugins.collectd')
+      should contain_ini_setting('collectd_enabled').with_setting('enabled')
+      should contain_ini_setting('collectd_enabled').with_value('false')
+    end
+    it 'udp' do
+      should contain_ini_setting('udp_enabled').with_section('input_plugins.udp')
+      should contain_ini_setting('udp_enabled').with_setting('enabled')
+      should contain_ini_setting('udp_enabled').with_value('false')
     end
   end
 end

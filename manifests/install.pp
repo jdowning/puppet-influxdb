1a69910cc501bcc6282229ef25c9ef92f6e6a25c
Roman Plessl <roman.plessl@nine.ch>
2015-01-07 16:04:07 +0100

    change install mechanism

--- manifests/install.pp
+++ manifests/install.pp
@@ -28,44 +28,46 @@ class influxdb::install {
           /64/    => "influxdb_${influxdb::version}_amd64.deb",
           default => "influxdb_${influxdb::version}_i386.deb",
         }
-        $package_source = "http://s3.amazonaws.com/influxdb/$package_source_name"
+        $package_source = "http://s3.amazonaws.com/influxdb/${package_source_name}"
         exec {
-          "influxdb_wget":
+          'influxdb_wget':
             command => "wget ${package_source} -O /tmp/${package_source_name}",
             path    => ['/bin', '/usr/bin'],
-            unless  => 'dpkg --list | grep influxdb';
+            unless  => 'dpkg --list influxdb',
 
-          "influxdb_dpkg":
-            command => "sudo dpkg -i /tmp/${package_source_name}",
+          'influxdb_dpkg':
+            command => "dpkg -i /tmp/${package_source_name}",
             path    => ['/bin', '/usr/bin'],
-            require => [ Exec["influxdb_wget"] ];
+            require => [ Exec['influxdb_wget'] ];
 
-          "influxdb_rm":
+          'influxdb_rm':
             command => "rm /tmp/${package_source_name}",
             path    => ['/bin', '/usr/bin'],
-            require => [ Exec["influxdb_dpkg"] ];
+            require => [ Exec['influxdb_dpkg'] ];
+
         }
       }
       'RedHat', 'Amazon': {
         $package_source_name = $::architecture ? {
-          /64/    => "http://s3.amazonaws.com/influxdb/influxdb-${influxdb::version}-1.x86_64.rpm",
-          default => "http://s3.amazonaws.com/influxdb/influxdb-${influxdb::version}-1.i686.rpm",
+          /64/    => "influxdb-${influxdb::version}-1.x86_64.rpm",
+          default => "influxdb-${influxdb::version}-1.i686.rpm",
         }
-        $package_source = "http://s3.amazonaws.com/influxdb/$package_source_name"
-        exec { "influxdb.rpm":
-          command => "sudo rpm -ivh $package_source",
+        $package_source = "http://s3.amazonaws.com/influxdb/${package_source_name}"
+        exec { 'influxdb.rpm':
+          command => "rpm -ivh ${package_source}",
           path    => ['/bin', '/usr/bin'],
-          unless  => 'rpm -qa | grep influxdb',
+          unless  => 'rpm -qa influxdb',
         }
       }
       default: {
-        fail('Only supports Debian or RedHat $::osfamily')
+        fail('Only supports Debian / Ubuntu or RedHat $::osfamily')
       }
     }
   }
-
-  # install / purge the package
-  package { 'influxdb':
-    ensure => $my_package_ensure,
-  } 
+  else {
+    # install / purge the package
+    package { 'influxdb':
+      ensure => $my_package_ensure,
+    }
+  }
 }

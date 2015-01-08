c5f9cef907b249649a7e6a899f495ce2273f22a7
Roman Plessl <roman.plessl@nine.ch>
2015-01-08 10:50:59 +0100

    improve test cases / coverage

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe 'influxdb::install' do
+  # default params case
   context 'installing from a repository' do
     let(:pre_condition) {
       'class{"influxdb":
@@ -32,4 +33,36 @@ describe 'influxdb::install' do
       )}
     end
   end
+  context 'installing from weburl' do
+    let(:pre_condition) {
+      'class{"influxdb":
+         install_from_repository => false,
+      }'
+    }
+    context 'on debian' do
+      let(:facts) {
+        {
+          :osfamily => 'Debian',
+          :architecture => 'x86_64',
+        }
+      }
+
+      it {
+        should contain_file('/opt/influxdb/versions/influxdb_from_web')
+      }
+    }
+    context 'on redhat' do
+      let(:facts) {
+        {
+          :osfamily => 'RedHat',
+          :architecture => 'x86_64',
+        }
+      }
+
+      it {
+        should contain_file('/opt/influxdb/versions/influxdb_from_web')
+      }
+    end
+  end
+
 end

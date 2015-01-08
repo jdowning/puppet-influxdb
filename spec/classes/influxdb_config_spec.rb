41839d08961760a0a67bcd5a3c79da54041de3ed
Roman Plessl <roman.plessl@nine.ch>
2015-01-08 15:09:38 +0100

    back to working baseline and retesting spec

--- spec/classes/influxdb_config_spec.rb
+++ spec/classes/influxdb_config_spec.rb
@@ -13,30 +13,5 @@ describe 'influxdb::config', :type => :class do
       }'
     }
 
-    context "on Debian based machines" do
-      let(:osfamily) { 'Debian' }
-      let(:operatingsystemmajrelease) { nil }
-      let(:lsbdistid) { 'Ubuntu' }
-      let(:lsbdistrelease) { '14.04' }
-      let(:facts) do
-      {
-        :osfamily => osfamily,
-        :operatingsystemmajrelease => operatingsystemmajrelease,
-        :lsbdistid => lsbdistid,
-        :lsbdistrelease => lsbdistrelease,
-      }
-      end
-
-      it {
-        should contain_file('/opt/influxdb/shared/config.toml').with({
-          :mode   => '0644',
-          :owner  => 'influxdb',
-          :group  => 'influxdb'
-        } )
-
-        should contain_file('/opt/influxdb/shared/config.toml').with_content(/^bind-address/)
-        should contain_file('/opt/influxdb/shared/config.toml').with_content(/^file\s=\s\/opt\/influxdb\/shared\/influxdb.log/)
-      }
-    end
   end
 end
--- spec/classes/influxdb_install_spec.rb
+++ spec/classes/influxdb_install_spec.rb
@@ -35,38 +35,4 @@ describe 'influxdb::install', :type => :class do
       )}
     end
   end
-
-  context 'installing from weburl' do
-    let(:pre_condition) {
-      'class{"influxdb":
-        ensure => "installed",
-        install_from_repository => false,
-      }'
-    }
-    context 'on debian' do
-      let(:facts) {
-        {
-          :osfamily => 'Debian',
-          :architecture => 'x86_64',
-        }
-      }
-      it {
-        should contain_file('/opt/influxdb/versions/influxdb_from_web')
-      }
-    end
-    context 'on redhat' do
-      let(:facts) {
-        {
-          :osfamily => 'RedHat',
-          :architecture => 'x86_64',
-        }
-      }
-      it {
-        should contain_file('/opt/influxdb/versions/influxdb_from_web')
-      }
-    end
-  end
-
-  it { should contain_file('/opt/influxdb').with('ensure' => 'directory') }
-
 end

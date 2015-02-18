0b7ffaec51065be49eaa0efe1c4c5d27d5b15ea0
ronen <narkisr@gmail.com>
2015-02-18 18:29:53 +0200

    success code is 201

--- manifests/database.pp
+++ manifests/database.pp
@@ -4,19 +4,20 @@ define influxdb::database(
   $pass = 'root'
 ) {
 
+
   if($ensure == 'present'){
     $db_exists = "curl \"${api::base}/db?u=root&p=${pass}\" | grep ${name}"
     exec{"create ${name}":
-      command => "${api::post} '${api::base}/db?u=root&p=${pass}' -d '{\"name\": \"${name}\"}' | grep 200",
+      command => "${api::post} '${api::base}/db?u=root&p=${pass}' -d '{\"name\": \"${name}\"}' | grep 201",
       user    => 'root',
       path    => ['/usr/bin', '/bin'],
-      onlyif  => [$api::port_open, "echo '! $db_exists' | bash"],
+      onlyif  => [$api::port_open, "echo '! ${db_exists}' | bash"],
       require => [Package['curl'], Service['influxdb']],
     }
   }
 
   if($ensure == 'absent'){
-    exec{"create ${name}":
+    exec{"delete ${name}":
       command => "${api::delete} '${api::base}/db/${name}?u=root&p=${pass}' | grep 200",
       user    => 'root',
       path    => ['/usr/bin', '/bin'],

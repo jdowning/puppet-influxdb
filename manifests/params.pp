4569c8510b2515ba5366177bf6b93f9e7e37797e
Roman Plessl <roman.plessl@nine.ch>
2015-08-11 10:27:58 +0200

    small identation fixes

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,11 +2,11 @@
 # DO NOT CALL DIRECTLY
 class influxdb::params {
   $ensure                  = 'installed'
-  $version                 = '0.9.1'
+  $version                 = '0.9.2'
   $install_from_repository = true
   $config_file             = '/etc/opt/influxdb/influxdb.conf'
 
-  $download_url               = undef
+  $download_url            = undef
   # general section of influxdb.conf
   $reporting_disabled      = false
 
